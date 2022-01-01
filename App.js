555021be28f9a531efbf39a731a9f91329c73013
gwak <jaewoo010207@gmail.com>
2022-01-01 22:46:17 +0900

    drop down

--- App.js
+++ App.js
@@ -6,7 +6,26 @@ import key from "./key";
 import SelectDropdown from "react-native-select-dropdown";
 
 const API_KEY = key;
-const countries = ["Egypt", "Canada", "Australia", "Ireland"];
+const CITY_LIST = {
+  korea: "한국",
+  seoul: "서울",
+  busan: "부산",
+  daegu: "대구",
+  incheon: "인천",
+  gwangju: "광주",
+  daejeon: "대전",
+  ulsan: "울산",
+  sejong: "세종",
+  gyeonggi: "경기",
+  gangwon: "강원",
+  chungbuk: "충북",
+  chungnam: "충남",
+  jeonbuk: "전북",
+  jeonnam: "전남",
+  gyeongbuk: "경북",
+  gyeongnam: "경남",
+  jeju: "제주",
+};
 
 export default function App() {
   const [region, setRegion] = useState({});
@@ -26,26 +45,10 @@ export default function App() {
     ).data;
     // console.log("getData for region", regionResponse);
     console.log(Object.keys(regionResponse));
-    const newCity = [
-      "korea",
-      "seoul",
-      "busan",
-      "daegu",
-      "incheon",
-      "gwangju",
-      "daejeon",
-      "ulsan",
-      "sejong",
-      "gyeonggi",
-      "gangwon",
-      "chungbuk",
-      "chungnam",
-      "jeonbuk",
-      "jeonnam",
-      "gyeongbuk",
-      "gyeongnam",
-      "jeju",
-    ];
+
+    const newCity = Object.values(CITY_LIST);
+    console.log(Object.values(CITY_LIST));
+
     // console.log("newcity", newCity);
     setCity(newCity);
     setTotal(totalResponse);
